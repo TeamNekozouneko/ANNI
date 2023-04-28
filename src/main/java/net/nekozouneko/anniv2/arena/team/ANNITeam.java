cb99aa5b54da8ad0b34e44ec4f8819fd49c29852
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-04-28 16:25:02 +0900

    Add: Team enum class

---
+++ src/main/java/net/nekozouneko/anniv2/arena/team/ANNITeam.java
@@ -0,0 +1,16 @@
+package net.nekozouneko.anniv2.arena.team;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum ANNITeam {
+
+    @SerializedName("red")
+    RED,
+    @SerializedName("blue")
+    BLUE,
+    @SerializedName("green")
+    GREEN,
+    @SerializedName("yellow")
+    YELLOW;
+
+}
