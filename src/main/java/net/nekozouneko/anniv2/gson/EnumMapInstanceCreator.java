90df82824e0731b6120ee965510f00cf8275c85e
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-04-28 16:20:21 +0900

    Add: add enummap instance creator

---
+++ src/main/java/net/nekozouneko/anniv2/gson/EnumMapInstanceCreator.java
@@ -0,0 +1,22 @@
+package net.nekozouneko.anniv2.gson;
+
+import com.google.gson.InstanceCreator;
+
+import java.lang.reflect.Type;
+import java.util.EnumMap;
+
+public class EnumMapInstanceCreator<K extends Enum<K>, V> implements InstanceCreator<EnumMap<K, V>> {
+
+    private final Class<K> enumType;
+
+    public EnumMapInstanceCreator(Class<K> enumType) {
+        super();
+        this.enumType = enumType;
+    }
+
+    @Override
+    public EnumMap<K, V> createInstance(Type type) {
+        return new EnumMap<>(enumType);
+    }
+
+}
