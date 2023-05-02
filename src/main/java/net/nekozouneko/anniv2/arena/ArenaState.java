39059ca7c2d339a5bf8b9cfd0ef450cb66fd62f1
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-02 13:53:56 +0900

    新たに#getId, #getTimerStateKeyを追加

--- src/main/java/net/nekozouneko/anniv2/arena/ArenaState.java
+++ src/main/java/net/nekozouneko/anniv2/arena/ArenaState.java
@@ -1,18 +1,36 @@
 package net.nekozouneko.anniv2.arena;
 
+import net.nekozouneko.anniv2.message.ANNIMessage;
+
 public enum ArenaState {
 
-    WAITING,
-    STARTING,
+    WAITING(-2, null),
+    STARTING(-1, null),
 
-    PHASE_ONE,
-    PHASE_TWO,
-    PHASE_THREE,
-    PHASE_FOUR,
-    PHASE_FIVE,
+    PHASE_ONE(1, ""),
+    PHASE_TWO(2, ""),
+    PHASE_THREE(3, ""),
+    PHASE_FOUR(4, ""),
+    PHASE_FIVE(5, ""),
 
-    GAME_OVER,
+    GAME_OVER(0, null),
 
-    STOPPED
+    STOPPED(-3, null);
+
+    private final int id;
+    private final String timerStateKey;
+
+    private ArenaState(int id, String timerStateKey) {
+        this.id = id;
+        this.timerStateKey = timerStateKey;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTimerStateKey() {
+        return timerStateKey;
+    }
 
 }
