7bc0bedbd3307d14952a6ff7d44eec628e400739
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-02 13:52:11 +0900

    Update: ワールドのコピーと安全にディレクトリを削除するメソッドを追加

--- src/main/java/net/nekozouneko/anniv2/util/FileUtil.java
+++ src/main/java/net/nekozouneko/anniv2/util/FileUtil.java
@@ -1,5 +1,6 @@
 package net.nekozouneko.anniv2.util;
 
+import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonWriter;
@@ -7,14 +8,22 @@ import net.nekozouneko.anniv2.arena.team.ANNITeam;
 import net.nekozouneko.anniv2.gson.EnumMapInstanceCreator;
 import net.nekozouneko.anniv2.map.Nexus;
 import net.nekozouneko.anniv2.map.SpawnLocation;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.EnumMap;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 public final class FileUtil {
 
-    private FileUtil() {}
+    private static final List<String> DO_NOT_COPY_WORLD_FILE = Arrays.asList("uid.dat", "session.lock");
+
+    private FileUtil() {
+    }
 
     public static Gson createGson() {
         GsonBuilder gb = new GsonBuilder()
@@ -47,8 +56,7 @@ public final class FileUtil {
             w.setIndent("    ");
             gson.toJson(obj, cls, w);
             w.flush();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             return false;
         }
 
@@ -69,11 +77,62 @@ public final class FileUtil {
                 )
         ) {
             return gson.fromJson(r, cls);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static boolean checkFileNameValidate(String name) {
+        return !name.matches(".*[\\\\/:*?\"<>|].*");
+    }
+
+    public static World copyWorld(World original, String name) throws IOException {
+        Path from = Paths.get(original.getName());
+        Path to = Paths.get(name);
+
+        Preconditions.checkArgument(!checkFileNameValidate(name), "Cant use character detected");
+        Preconditions.checkState(Bukkit.getWorld(name) != null, "World \"" + name + "\" is already exists");
+        if (Files.exists(to)) throw new FileAlreadyExistsException(name + " is already exists");
+
+        Files.walkFileTree(from, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Path dir2 = to.resolve(from.relativize(dir));
+                if (!Files.exists(dir2)) Files.createDirectory(dir2);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path file2 = to.resolve(from.relativize(file));
+
+                if (!(Files.exists(file2) || DO_NOT_COPY_WORLD_FILE.contains(file2.getFileName().toString()))) {
+                    Files.copy(file, file2, StandardCopyOption.COPY_ATTRIBUTES);
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        return WorldCreator.name(name).createWorld();
+    }
+
+    public static void safeDeleteIfExists(Path path) throws IOException {
+        safeDeleteIfExists(path.toFile());
+    }
+
+    public static void safeDeleteIfExists(File file) throws IOException {
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            if (f.isDirectory()) safeDeleteIfExists(f);
+            else {
+                try {
+                    Files.deleteIfExists(f.toPath());
+                }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+        }
+        Files.deleteIfExists(file.toPath());
     }
 
 }
