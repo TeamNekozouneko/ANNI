4be1587724c57888b98195efbeb4b3bdcab79162
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-05 15:27:52 +0900

    getUsageから引数除去とexecute関数はbooleanを返すように

--- src/main/java/net/nekozouneko/anniv2/command/ASubCommand.java
+++ src/main/java/net/nekozouneko/anniv2/command/ASubCommand.java
@@ -1,16 +1,15 @@
 package net.nekozouneko.anniv2.command;
 
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.util.List;
 
 public abstract class ASubCommand {
 
-    public abstract void execute(CommandSender sender, List<String> args);
+    public abstract boolean execute(CommandSender sender, List<String> args);
 
     public abstract List<String> tabComplete(CommandSender sender, List<String> args);
 
-    public abstract String getUsage(Command cmd, String label);
+    public abstract String getUsage();
 
 }
