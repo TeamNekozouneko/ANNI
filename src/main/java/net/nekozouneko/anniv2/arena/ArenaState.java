d512b5dbc61af5603de1765d04d247cdd8d535a6
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-13 00:59:14 +0900

    [Update] 新たなメソッド追加

--- src/main/java/net/nekozouneko/anniv2/arena/ArenaState.java
+++ src/main/java/net/nekozouneko/anniv2/arena/ArenaState.java
@@ -2,25 +2,31 @@ package net.nekozouneko.anniv2.arena;
 
 public enum ArenaState {
 
-    WAITING(-2, null),
-    STARTING(-1, null),
+    WAITING(-2, null, false, 0, null),
+    STARTING(-1, null, false, 60, null),
 
-    PHASE_ONE(1, "bossbar.timer.state.1"),
-    PHASE_TWO(2, "bossbar.timer.state.2"),
-    PHASE_THREE(3, "bossbar.timer.state.3"),
-    PHASE_FOUR(4, "bossbar.timer.state.4"),
-    PHASE_FIVE(5, "bossbar.timer.state.5"),
+    PHASE_FIVE(5, "bossbar.timer.state.5", true, 0, null),
+    PHASE_FOUR(4, "bossbar.timer.state.4", true, 600, PHASE_FIVE),
+    PHASE_THREE(3, "bossbar.timer.state.3", true, 600, PHASE_FOUR),
+    PHASE_TWO(2, "bossbar.timer.state.2", true, 600, PHASE_THREE),
+    PHASE_ONE(1, "bossbar.timer.state.1", false, 600, PHASE_TWO),
 
-    GAME_OVER(0, "bossbar.timer.state.restarting"),
+    GAME_OVER(0, "bossbar.timer.state.restarting", false, 30, null),
 
-    STOPPED(-3, null);
+    STOPPED(-3, null, false, 0, null);
 
     private final int id;
     private final String timerStateKey;
+    private final boolean canDestroyNexus;
+    private final long nextPhaseIn;
+    private final ArenaState nextPhase;
 
-    private ArenaState(int id, String timerStateKey) {
+    private ArenaState(int id, String timerStateKey, boolean canDestroyNexus, long nextPhaseIn, ArenaState nextPhase) {
         this.id = id;
         this.timerStateKey = timerStateKey;
+        this.canDestroyNexus = canDestroyNexus;
+        this.nextPhaseIn = nextPhaseIn;
+        this.nextPhase = nextPhase;
     }
 
     public int getId() {
@@ -29,6 +35,18 @@ public enum ArenaState {
 
     public String getTimerStateKey() {
         return timerStateKey;
+    }
+
+    public boolean canDestroyNexus() {
+        return canDestroyNexus;
+    }
+
+    public long nextPhaseIn() {
+        return nextPhaseIn;
+    }
+
+    public ArenaState nextPhase() {
+        return nextPhase;
     }
 
 }
