0638d117f9b36c4a18fef8cdbbc707cfe2dbbcbd
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-14 00:17:22 +0900

    [Update] ネクサスの破壊処理を追加

--- src/main/java/net/nekozouneko/anniv2/listener/BlockBreakListener.java
+++ src/main/java/net/nekozouneko/anniv2/listener/BlockBreakListener.java
@@ -40,22 +40,51 @@ public class BlockBreakListener implements Listener {
         ANNIPlugin plugin = ANNIPlugin.getInstance();
         ANNIArena current = plugin.getCurrentGame();
 
-        if (current.getState().canDestroyNexus()) {
+        if (current.getState().getId() > 0) {
+            // ネクサス
             for (Map.Entry<ANNITeam, Nexus> ent : current.getMap().getNexuses().entrySet()) {
+                if (!current.getTeams().containsKey(ent.getKey())) continue;
                 Location loc = BukkitAdapter.adapt(
                         current.getCopyWorld(),
                         ent.getValue().getLocation()
                 );
                 if (e.getBlock().getLocation().equals(loc)) {
-                    current.damageNexusHealth(ent.getKey(), 1, e.getPlayer());
-                    if (current.isNexusLost(ent.getKey())) {
-                        Nexus.finalDestroyEffects(loc);
-                        Bukkit.getScheduler().runTask(plugin, () -> e.getBlock().setType(Material.BEDROCK));
+                    if (current.isNexusLost(ent.getKey())) e.setCancelled(true);
+
+                    // 破壊しようとしてるのは自チームかどうか
+                    if (current.getTeamByPlayer(e.getPlayer()).equals(ent.getKey())) {
+                        e.getPlayer().sendMessage(plugin.getMessageManager().build("nexus.cant_destroy_self"));
+                        e.setCancelled(true);
                     }
-                    else {
-                        Nexus.destroyEffects(loc);
-                        Bukkit.getScheduler().runTask(plugin, () -> e.getBlock().setType(Material.END_STONE));
+                    else if (current.getState().canDestroyNexus()) { // 現在のフェーズで破壊できるなら
+                        current.damageNexusHealth(ent.getKey(), 1, e.getPlayer());
+                        if (current.isNexusLost(ent.getKey())) {
+                            Nexus.finalDestroyEffects(loc);
+                            Bukkit.getScheduler().runTask(plugin, () -> e.getBlock().setType(Material.BEDROCK));
+                        } else {
+                            Nexus.destroyEffects(loc);
+                            Bukkit.getScheduler().runTaskLater(plugin, () -> e.getBlock().setType(Material.END_STONE), 3);
+                        }
                     }
+                    else { // 現在のフェーズで破壊できないなら
+                        e.getPlayer().sendMessage(plugin.getMessageManager().build("nexus.now_cant_destroy"));
+                        e.setCancelled(true);
+                    }
+
+                    return;
+                }
+            }
+
+            switch (e.getBlock().getType()) {
+                case COAL_ORE:
+                case DIAMOND_ORE:
+                case EMERALD_ORE:
+                case GOLD_ORE:
+                case IRON_ORE:
+                case LAPIS_ORE:
+                case NETHER_GOLD_ORE:
+                case NETHER_QUARTZ_ORE:
+                case REDSTONE_ORE: {
                     break;
                 }
             }
