73ffc85c6d0e4c7fe00ce26cf295a7b281771be8
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-14 00:18:07 +0900

    [Update] importを最適化

--- src/main/java/net/nekozouneko/anniv2/board/BoardManager.java
+++ src/main/java/net/nekozouneko/anniv2/board/BoardManager.java
@@ -5,7 +5,6 @@ import net.nekozouneko.anniv2.ANNIPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashMap;
