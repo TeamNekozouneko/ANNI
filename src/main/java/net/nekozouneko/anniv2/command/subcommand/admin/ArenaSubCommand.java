3502e3d8f5ec1ee07910e5d750118894d2bad533
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-15 17:02:47 +0900

    [Add] arenaのサブコマンドを実装。一部コマンドをdebugからコピー予定

---
+++ src/main/java/net/nekozouneko/anniv2/command/subcommand/admin/ArenaSubCommand.java
@@ -0,0 +1,82 @@
+package net.nekozouneko.anniv2.command.subcommand.admin;
+
+import com.google.common.base.Enums;
+import net.nekozouneko.anniv2.ANNIPlugin;
+import net.nekozouneko.anniv2.arena.ANNIArena;
+import net.nekozouneko.anniv2.arena.team.ANNITeam;
+import net.nekozouneko.anniv2.command.ASubCommand;
+import net.nekozouneko.anniv2.message.MessageManager;
+import net.nekozouneko.anniv2.util.CmdUtil;
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArenaSubCommand extends ASubCommand {
+
+    private final ANNIPlugin plugin = ANNIPlugin.getInstance();
+    private final MessageManager mm = plugin.getMessageManager();
+
+    @Override
+    public boolean execute(CommandSender sender, List<String> args) {
+        if (args.size() == 0) {
+            return false;
+        }
+
+        ANNIArena current = plugin.getCurrentGame();
+
+        switch (args.get(0)) {
+            case "disable-team": {
+                if (current.getTeams().size() <= 2) {
+                    sender.sendMessage(mm.build("command.err.no_more_teams_can_be_disabled"));
+                    return true;
+                }
+
+                ANNITeam at = Enums.getIfPresent(ANNITeam.class, args.get(1)).orNull();
+                if (at == null) sender.sendMessage(mm.build("command.err.team_undefined"), args.get(1));
+                else {
+                    current.disableTeam(at);
+                    sender.sendMessage(mm.build("command.arena.disabled_team", at.getTeamName()));
+                }
+                break;
+            }
+            case "enable-team": {
+                ANNITeam at = Enums.getIfPresent(ANNITeam.class, args.get(1)).orNull();
+                if (at == null) sender.sendMessage(mm.build("command.err.team_undefined"), args.get(1));
+                else {
+                    current.enableTeam(at);
+                    sender.sendMessage(mm.build("command.arena.enabled_team", at.getTeamName()));
+                }
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, List<String> args) {
+        if (args.size() == 1) {
+            return CmdUtil.simpleTabComplete(args.get(0), "disable-team", "enable-team");
+        }
+
+        switch (args.get(0)) {
+            case "disable-team":
+            case "enable-team":
+                return CmdUtil.simpleTabComplete(
+                        args.get(1),
+                        Arrays.stream(ANNITeam.values())
+                                .map(ANNITeam::name)
+                                .collect(Collectors.toList())
+                );
+            default: return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public String getUsage() {
+        return "(disable-team|enable-team) [<args>]";
+    }
+}
