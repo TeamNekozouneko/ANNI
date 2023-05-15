431a11e97e3544433f71623008db0c636e5961b6
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-15 17:07:00 +0900

    [Update] チーム名とprefixを取得できるように変更

--- src/main/java/net/nekozouneko/anniv2/arena/team/ANNITeam.java
+++ src/main/java/net/nekozouneko/anniv2/arena/team/ANNITeam.java
@@ -1,16 +1,33 @@
 package net.nekozouneko.anniv2.arena.team;
 
 import com.google.gson.annotations.SerializedName;
+import net.nekozouneko.anniv2.ANNIPlugin;
 
 public enum ANNITeam {
 
     @SerializedName("red")
-    RED,
+    RED("team.red.display", "team.red.prefix"),
     @SerializedName("blue")
-    BLUE,
+    BLUE("team.blue.display", "team.blue.prefix"),
     @SerializedName("green")
-    GREEN,
+    GREEN("team.green.display", "team.green.prefix"),
     @SerializedName("yellow")
-    YELLOW;
+    YELLOW("team.yellow.display", "team.yellow.prefix");
+
+    private final String name;
+    private final String prefix;
+
+    private ANNITeam(String name, String prefix) {
+        this.name = name;
+        this.prefix = prefix;
+    }
+
+    public String getTeamName() {
+        return ANNIPlugin.getInstance().getMessageManager().build(name);
+    }
+
+    public String getTeamPrefix() {
+        return ANNIPlugin.getInstance().getMessageManager().build(prefix);
+    }
 
 }
