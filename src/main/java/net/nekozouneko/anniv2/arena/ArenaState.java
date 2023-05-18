dddfc5d1472f46263f7c25e23b00e0a2428dba3a
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-18 23:03:59 +0900

    [Update] descriptionを追加

--- src/main/java/net/nekozouneko/anniv2/arena/ArenaState.java
+++ src/main/java/net/nekozouneko/anniv2/arena/ArenaState.java
@@ -2,39 +2,46 @@ package net.nekozouneko.anniv2.arena;
 
 public enum ArenaState {
 
-    WAITING(-2, null, null, 0, null),
-    STARTING(-1, null, null, 60, null),
+    WAITING(-2, null, null, null, 0, null),
+    STARTING(-1, null, null, null, 60, null),
 
-    PHASE_FIVE(5, "bossbar.timer.state.5", 2, 0, null),
-    PHASE_FOUR(4, "bossbar.timer.state.4", 2, 600, PHASE_FIVE),
-    PHASE_THREE(3, "bossbar.timer.state.3", 1, 600, PHASE_FOUR),
-    PHASE_TWO(2, "bossbar.timer.state.2", 1, 600, PHASE_THREE),
-    PHASE_ONE(1, "bossbar.timer.state.1", null, 600, PHASE_TWO),
+    PHASE_FIVE(5, "phase.name.5", "phase.description.5", 2, 0, null),
+    PHASE_FOUR(4, "phase.name.4", "phase.description.4", 2, 600, PHASE_FIVE),
+    PHASE_THREE(3, "phase.name.3", "phase.description.3", 1, 600, PHASE_FOUR),
+    PHASE_TWO(2, "phase.name.2", "phase.description.2", 1, 600, PHASE_THREE),
+    PHASE_ONE(1, "phase.name.1", "phase.description.1", null, 600, PHASE_TWO),
 
-    GAME_OVER(0, "bossbar.timer.state.restarting", null, 30, null),
+    GAME_OVER(0, "bossbar.timer.state.restarting", null, null, 30, null),
 
-    STOPPED(-3, null, null, 0, null);
+    STOPPED(-3, null, null, null, 0, null);
 
     private final int id;
-    private final String timerStateKey;
+    private final String name;
+    private final String description;
+
     private final Integer nexusDamage;
     private final long nextPhaseIn;
     private final ArenaState nextPhase;
 
-    private ArenaState(int id, String timerStateKey, Integer nexusDamage, long nextPhaseIn, ArenaState nextPhase) {
+    private ArenaState(int id, String name, String desc, Integer nexusDamage, long nextPhaseIn, ArenaState nextPhase) {
         this.id = id;
-        this.timerStateKey = timerStateKey;
+        this.name = name;
         this.nexusDamage = nexusDamage;
         this.nextPhaseIn = nextPhaseIn;
         this.nextPhase = nextPhase;
+        this.description = desc;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getTimerStateKey() {
-        return timerStateKey;
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public boolean canDestroyNexus() {
