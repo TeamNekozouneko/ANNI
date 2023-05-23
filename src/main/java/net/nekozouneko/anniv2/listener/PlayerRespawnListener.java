332e448a52cbf3e3e40b45dd520be90a6ea2b927
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-23 23:33:20 +0900

    リスキル対策を追加

--- src/main/java/net/nekozouneko/anniv2/listener/PlayerRespawnListener.java
+++ src/main/java/net/nekozouneko/anniv2/listener/PlayerRespawnListener.java
@@ -3,9 +3,13 @@ package net.nekozouneko.anniv2.listener;
 import net.nekozouneko.anniv2.ANNIPlugin;
 import net.nekozouneko.anniv2.arena.ANNIArena;
 import net.nekozouneko.anniv2.arena.team.ANNITeam;
+import net.nekozouneko.anniv2.kit.ANNIKit;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerRespawnListener implements Listener {
     
@@ -20,11 +24,28 @@ public class PlayerRespawnListener implements Listener {
 
             if (at != null) {
                 if (current.isNexusLost(at)) {
-                    if (plugin.getLobby() != null) e.setRespawnLocation(plugin.getLobby());
+                    e.getPlayer().getInventory().clear();
+                    e.getPlayer().getEnderChest().clear();
+                    e.getPlayer().setLevel(0);
+                    e.getPlayer().setExp(0);
+
+                    if (plugin.getLobby() != null)
+                        e.setRespawnLocation(plugin.getLobby());
                 }
                 else e.setRespawnLocation(current.getMap().getSpawnOrDefault(at).toLocation(current.getCopyWorld()));
             }
             else e.setRespawnLocation(current.getMap().getDefaultSpawn().toLocation(current.getCopyWorld()));
+
+            // リスキル対策で 耐性4 (80%カット) と スピード 1 をリスポーン時付与
+            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 4, false, false, true));
+            e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 200, 1, false, false, true));
+
+            e.getPlayer().setGameMode(GameMode.SURVIVAL);
+            e.getPlayer().getInventory().setContents(
+                    current.getTeamByPlayer(e.getPlayer()) != null ?
+                        ANNIKit.teamColor(current.getKit(e.getPlayer()), current.getTeamByPlayer(e.getPlayer()))
+                        : current.getKit(e.getPlayer()).getKitContents()
+            );
         }
         else {
             if (plugin.getLobby() != null) e.setRespawnLocation(plugin.getLobby());
