14fac62886e09371da80b29607c28cb0b933c0c8
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-23 23:37:59 +0900

    チャットシステムを作成

---
+++ src/main/java/net/nekozouneko/anniv2/listener/AsyncPlayerChatListener.java
@@ -0,0 +1,77 @@
+package net.nekozouneko.anniv2.listener;
+
+import com.google.common.base.Strings;
+import net.nekozouneko.anniv2.ANNIPlugin;
+import net.nekozouneko.anniv2.arena.team.ANNITeam;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.scoreboard.Team;
+
+public class AsyncPlayerChatListener implements Listener {
+
+    private final ANNIPlugin plugin = ANNIPlugin.getInstance();
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        ANNITeam at = plugin.getCurrentGame().getTeamByPlayer(e.getPlayer());
+        if (at != null && !e.getMessage().startsWith("!")) {
+            Team t = plugin.getCurrentGame().getTeam(at);
+
+            try {
+                e.getRecipients().clear();
+                e.getRecipients().addAll(plugin.getCurrentGame().getTeamPlayers(at));
+                String form = plugin.getMessageManager().build("chat.team.format",
+                        t != null ? Strings.nullToEmpty(t.getPrefix()) + t.getColor() : ""
+                );
+                e.setFormat(form);
+                plugin.getLogger().info(String.format(form, e.getPlayer().getName(), e.getMessage()));
+            }
+            catch (Exception e1) {
+                e.setCancelled(true);
+                String mes = plugin.getMessageManager().build("chat.team",
+                        t != null ?
+                                Strings.nullToEmpty(t.getPrefix()) + t.getColor() + e.getPlayer().getName()
+                                : e.getPlayer().getName(),
+                        e.getMessage()
+                );
+                plugin.getCurrentGame().broadcast(mes, at);
+                plugin.getLogger().info(mes);
+            }
+        }
+        else {
+            String prefix;
+            String username;
+            if (at != null) {
+                e.setMessage(e.getMessage().substring(1));
+                Team t = plugin.getCurrentGame().getTeam(at);
+
+                prefix = Strings.nullToEmpty(t.getPrefix()) + t.getColor();
+                username = prefix + e.getPlayer().getName();
+            }
+            else {
+                prefix = "";
+                username = e.getPlayer().getName();
+            }
+
+
+            try {
+                e.getRecipients().clear();
+                e.getRecipients().addAll(plugin.getCurrentGame().getTeamPlayers(at));
+                String form = plugin.getMessageManager().build("chat.global.format", prefix);
+                e.setFormat(form);
+                plugin.getLogger().info(String.format(form, e.getPlayer().getName(), e.getMessage()));
+            }
+            catch (Exception e1) {
+                e.setCancelled(true);
+                String mes = plugin.getMessageManager().build("chat.global",
+                        username,
+                        e.getMessage()
+                );
+                plugin.getCurrentGame().broadcast(mes, at);
+                plugin.getLogger().info(mes);
+            }
+        }
+    }
+
+}
