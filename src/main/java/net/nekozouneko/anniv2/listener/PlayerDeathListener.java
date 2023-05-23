d421f03744018b04606f6091dee4ef344a2927dc
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-05-23 23:33:58 +0900

    死んだ際キットのアイテムをドロップしないように

--- src/main/java/net/nekozouneko/anniv2/listener/PlayerDeathListener.java
+++ src/main/java/net/nekozouneko/anniv2/listener/PlayerDeathListener.java
@@ -2,9 +2,17 @@ package net.nekozouneko.anniv2.listener;
 
 import net.nekozouneko.anniv2.ANNIPlugin;
 import net.nekozouneko.anniv2.message.MessageManager;
+import net.nekozouneko.anniv2.util.CmnUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
 
 public class PlayerDeathListener implements Listener {
 
@@ -13,26 +21,44 @@ public class PlayerDeathListener implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
+        ChatColor cc = CmnUtil.getJoinedTeam(e.getEntity()) != null ?
+                CmnUtil.getJoinedTeam(e.getEntity()).getColor() : ChatColor.WHITE;
+
         if (e.getEntity().getKiller() != null) {
-            e.setDeathMessage(mm.build("kill.default", e.getEntity().getName(), e.getEntity().getKiller().getName()));
+            ChatColor cck = CmnUtil.getJoinedTeam(e.getEntity().getKiller()) != null ?
+                    CmnUtil.getJoinedTeam(e.getEntity().getKiller()).getColor() : ChatColor.WHITE;
+            e.setDeathMessage(mm.build("kill.default", cc + e.getEntity().getName(), cck + e.getEntity().getKiller().getName()));
         }
         else {
             switch (e.getEntity().getLastDamageCause().getCause()) {
                 case FALL:
-                    e.setDeathMessage(mm.build("kill.fall", e.getEntity().getName()));
+                    e.setDeathMessage(mm.build("kill.fall", cc + e.getEntity().getName()));
                     break;
                 case VOID:
-                    e.setDeathMessage(mm.build("kill.void", e.getEntity().getName()));
+                    e.setDeathMessage(mm.build("kill.void", cc + e.getEntity().getName()));
                     break;
                 case ENTITY_ATTACK:
-                    e.setDeathMessage(mm.build("kill.mob", e.getEntity().getName()));
+                    e.setDeathMessage(mm.build("kill.mob", cc + e.getEntity().getName()));
                     break;
                 default: {
-                    e.setDeathMessage(mm.build("kill.game", e.getEntity().getName()));
+                    e.setDeathMessage(mm.build("kill.game", cc + e.getEntity().getName()));
                     break;
                 }
             }
         }
+
+        // {kit-item: 1} じゃないアイテムをドロップさせる。
+        new LinkedHashSet<>(e.getDrops()).stream()
+                .filter(Objects::nonNull) // nullじゃないかつ
+                .filter(is -> { // {kit-item:1}なら
+                    PersistentDataContainer pdc = is.getItemMeta().getPersistentDataContainer();
+
+                    return pdc.getOrDefault(
+                            new NamespacedKey(ANNIPlugin.getInstance(), "kit-item"),
+                            PersistentDataType.INTEGER, 0
+                    ) == 1;
+                })
+                .forEach(e.getDrops()::remove); // ドロップするアイテムから削除
     }
 
 }
