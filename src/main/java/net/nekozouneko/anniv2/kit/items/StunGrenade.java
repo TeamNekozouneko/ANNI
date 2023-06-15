7478587b2e896ef09aac2cb7bc44086957edff73
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-06-15 10:35:38 +0900

    スタングレネードを強化

--- src/main/java/net/nekozouneko/anniv2/kit/items/StunGrenade.java
+++ src/main/java/net/nekozouneko/anniv2/kit/items/StunGrenade.java
@@ -121,7 +121,7 @@ public class StunGrenade implements Listener {
                         }
                     }
 
-                    hit.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 1));
+                    hit.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 2));
                     hit.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20, 1));
                 }
             }
