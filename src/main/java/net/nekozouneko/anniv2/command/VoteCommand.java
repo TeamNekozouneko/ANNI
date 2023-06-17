6df2e3078f6f5cb3d74aee0b3f0c8a7d428be486
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-06-18 00:04:04 +0900

    細かい修正

--- src/main/java/net/nekozouneko/anniv2/command/VoteCommand.java
+++ src/main/java/net/nekozouneko/anniv2/command/VoteCommand.java
@@ -10,8 +10,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,7 +21,7 @@ public class VoteCommand implements CommandExecutor, TabCompleter {
     private final MessageManager mm = plugin.getMessageManager();
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(mm.build("command.err.player_only"));
             return true;
@@ -52,9 +50,9 @@ public class VoteCommand implements CommandExecutor, TabCompleter {
         return true;
     }
 
-    @Nullable
+    
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length == 1 && VoteManager.hasChoices(plugin.getCurrentGame().getId())) {
             return CmdUtil.simpleTabComplete(
                     args[0],
