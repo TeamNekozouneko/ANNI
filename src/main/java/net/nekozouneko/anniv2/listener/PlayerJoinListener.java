9cfb80852aec90f6fe023f85f1919ef5d9b906b3
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-08-04 20:10:05 +0900

    優先順位を設定

--- src/main/java/net/nekozouneko/anniv2/listener/PlayerJoinListener.java
+++ src/main/java/net/nekozouneko/anniv2/listener/PlayerJoinListener.java
@@ -9,7 +9,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
 
-    @EventHandler( priority = EventPriority.LOWEST)
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onJoin(PlayerJoinEvent e) {
         Bukkit.getScheduler().runTask(ANNIPlugin.getInstance(), () ->
                 ANNIPlugin.getInstance().getCurrentGame().join(e.getPlayer())
