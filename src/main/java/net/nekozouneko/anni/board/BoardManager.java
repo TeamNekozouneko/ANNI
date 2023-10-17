c194f6cb36057519eeb1c4a013dab80015efee26
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-10-17 18:17:12 +0900

    FastBoardを2.0.1にアップデート

--- src/main/java/net/nekozouneko/anni/board/BoardManager.java
+++ src/main/java/net/nekozouneko/anni/board/BoardManager.java
@@ -7,14 +7,47 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BoardManager implements Listener {
 
+    public class ANNIFastBoard extends FastBoard {
+
+        public ANNIFastBoard(Player player) {
+            super(player);
+        }
+
+        @Override
+        public boolean hasLinesMaxLength() {
+            try {
+                Class<?> via = Class.forName("com.viaversion.viaversion.api.Via");
+                Method getApi = via.getMethod("getAPI"); // com.viaversion.viaversion.api.Via#getAPI()
+                Object viaApi = getApi.invoke(null);
+
+                // com.viaversion.viaversion.api.Via#getAPI()#getPlayerVersion(Player)
+                Method getpv = viaApi.getClass().getMethod("getPlayerVersion", Player.class);
+
+                // com.viaversion.viaversion.api.protocol.version.ProtocolVersion.V1_13#getVersion()
+                Class<?> prov = Class.forName("com.viaversion.viaversion.api.protocol.version.ProtocolVersion");
+                Field v1_13Field = prov.getField("v1_13");
+                Object v1_13inst = v1_13Field.get(null);
+                Method getVer = v1_13inst.getClass().getMethod("getVersion");
+
+                return (int) getpv.invoke(viaApi, getPlayer()) < (int) getVer.invoke(v1_13inst);
+            }
+            catch (Exception ignored) {}
+
+            return super.hasLinesMaxLength();
+        }
+
+    }
+
     private final ANNIPlugin plugin;
 
-    private final Map<Player, FastBoard> boards = new HashMap<>();
+    private final Map<Player, ANNIFastBoard> boards = new HashMap<>();
 
     public BoardManager(ANNIPlugin plugin) {
         this.plugin = plugin;
@@ -22,13 +55,13 @@ public class BoardManager implements Listener {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    public FastBoard create(Player player) {
+    public ANNIFastBoard create(Player player) {
         delete(player);
-        return boards.put(player, new FastBoard(player));
+        return boards.put(player, new ANNIFastBoard(player));
     }
 
-    public FastBoard get(Player player) {
-        return boards.getOrDefault(player, new FastBoard(player));
+    public ANNIFastBoard get(Player player) {
+        return boards.getOrDefault(player, new ANNIFastBoard(player));
     }
 
     public void delete(Player player) {
@@ -46,5 +79,4 @@ public class BoardManager implements Listener {
     public void onQuit(PlayerQuitEvent e) {
         delete(e.getPlayer());
     }
-
 }
