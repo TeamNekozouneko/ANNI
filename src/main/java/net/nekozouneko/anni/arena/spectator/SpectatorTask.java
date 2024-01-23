837c689bb6256ba7c770b1308891e6caa4e2f6dc
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2024-01-23 11:54:18 +0900

    SpectatorTaskのコードを改善

--- src/main/java/net/nekozouneko/anni/arena/spectator/SpectatorTask.java
+++ src/main/java/net/nekozouneko/anni/arena/spectator/SpectatorTask.java
@@ -4,7 +4,6 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.nekozouneko.anni.ANNIPlugin;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -13,30 +12,33 @@ public class SpectatorTask extends BukkitRunnable {
 
     @Override
     public void run() {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            if (SpectatorManager.isSpectating(p)) {
-                p.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 40, 255, false, false, true));
-                p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 40, 1, false, false, true));
-                p.setAllowFlight(true);
-                p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ANNIPlugin.getInstance().getMessageManager()
-                        .build("actionbar.spectator_mode")
-                ));
+        Bukkit.getOnlinePlayers().forEach(player ->
+            Bukkit.getOnlinePlayers().forEach(watcher -> {
+                if (player.equals(watcher)) return;
 
-                Bukkit.getOnlinePlayers().forEach(p2 -> {
-                    if (!p2.equals(p)) {
-                        p2.hidePlayer(ANNIPlugin.getInstance(), p);
+                if (SpectatorManager.isSpectating(player)) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 40, 255, false, false, true));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 40, 1, false, false, true));
+                    player.setAllowFlight(true);
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ANNIPlugin.getInstance().getMessageManager()
+                            .build("actionbar.spectator_mode")
+                    ));
+
+                    if (SpectatorManager.isWatchable(player)) {
+                        if (watcher.canSee(player)) return;
+                        watcher.showPlayer(ANNIPlugin.getInstance(), player);
                     }
-                    if (SpectatorManager.isWatchable(p2)) {
-                        p2.showPlayer(ANNIPlugin.getInstance(), p);
+                    else {
+                        if (!watcher.canSee(player)) return;
+                        watcher.hidePlayer(ANNIPlugin.getInstance(), player);
                     }
-                });
-            }
-            else {
-                Bukkit.getOnlinePlayers().forEach(p2 -> {
-                    p2.showPlayer(ANNIPlugin.getInstance(), p);
-                });
-            }
-        }
+                }
+                else {
+                    if (watcher.canSee(player)) return;
+                    watcher.showPlayer(ANNIPlugin.getInstance(), player);
+                }
+            })
+        );
     }
 
 }
