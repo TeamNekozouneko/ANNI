be6e033fdf4a4f6891ade85960bf09fdeef266c2
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2024-02-09 18:23:41 +0900

    キーがnullになりマップがロードされない問題を修正 チームの領域を実装

--- src/main/java/net/nekozouneko/anni/map/ANNIMap.java
+++ src/main/java/net/nekozouneko/anni/map/ANNIMap.java
@@ -22,8 +22,9 @@ public class ANNIMap {
     @Getter
     private final String world;
 
-    private final EnumMap<ANNITeam, Nexus> nexus = new EnumMap<>(ANNITeam.class);
-    private final EnumMap<ANNITeam, SpawnLocation> spawn = new EnumMap<>(ANNITeam.class);
+    private EnumMap<ANNITeam, Nexus> nexus = new EnumMap<>(ANNITeam.class);
+    private EnumMap<ANNITeam, SpawnLocation> spawn = new EnumMap<>(ANNITeam.class);
+    private EnumMap<ANNITeam, String> team_region = new EnumMap<>(ANNITeam.class);
 
     @Getter
     private SpawnLocation defaultSpawn;
@@ -45,6 +46,7 @@ public class ANNIMap {
                 getBukkitWorld() != null
                 && nexus.size() >= 4
                 && spawn.size() >= 4
+                && team_region.size() >= 4
         );
     }
 
@@ -52,25 +54,46 @@ public class ANNIMap {
         return Bukkit.getWorld(world);
     }
 
+    public String getTeamRegion(ANNITeam team) {
+        if (team_region == null) team_region = new EnumMap<>(ANNITeam.class);
+        return team_region.get(team);
+    }
+
+    public Map<ANNITeam, String> getTeamRegions() {
+        if (team_region == null) team_region = new EnumMap<>(ANNITeam.class);
+        return Collections.unmodifiableMap(team_region);
+    }
+
+    public void setTeamRegion(ANNITeam team, String region) {
+        if (team_region == null) team_region = new EnumMap<>(ANNITeam.class);
+        team_region.put(team, region);
+    }
+
     public Nexus getNexus(ANNITeam team) {
+        if (nexus == null) nexus = new EnumMap<>(ANNITeam.class);
         return nexus.get(team);
     }
 
     public Map<ANNITeam, Nexus> getNexuses() {
+        if (nexus == null) nexus = new EnumMap<>(ANNITeam.class);
         return Collections.unmodifiableMap(nexus);
     }
 
     public SpawnLocation getSpawn(ANNITeam team) {
         Preconditions.checkNotNull(team);
 
+        if (spawn == null) spawn = new EnumMap<>(ANNITeam.class);
+
         return spawn.get(team);
     }
 
     public SpawnLocation getSpawnOrDefault(ANNITeam team) {
+        if (spawn == null) spawn = new EnumMap<>(ANNITeam.class);
         return spawn.getOrDefault(team, defaultSpawn);
     }
 
     public Map<ANNITeam, SpawnLocation> getSpawns() {
+        if (spawn == null) spawn = new EnumMap<>(ANNITeam.class);
         return Collections.unmodifiableMap(spawn);
     }
 
@@ -78,23 +101,28 @@ public class ANNIMap {
         defaultSpawn = SpawnLocation.fromLocation(location);
     }
 
+    @SuppressWarnings("LombokSetterMayBeUsed")
     public void setDefaultSpawn(SpawnLocation location) {
         defaultSpawn = location;
     }
 
     public void setNexus(ANNITeam team, Nexus nexus) {
+        if (this.nexus == null) this.nexus = new EnumMap<>(ANNITeam.class);
         this.nexus.put(team, nexus);
     }
 
     public void setNexus(ANNITeam team, Location location) {
+        if (nexus == null) nexus = new EnumMap<>(ANNITeam.class);
         nexus.put(team, new Nexus(location));
     }
 
     public void setNexus(ANNITeam team, BlockVector3 vector3) {
+        if (nexus == null) nexus = new EnumMap<>(ANNITeam.class);
         nexus.put(team, new Nexus(vector3));
     }
 
     public void setSpawn(ANNITeam team, SpawnLocation slocation) {
+        if (spawn == null) spawn = new EnumMap<>(ANNITeam.class);
         spawn.put(team, slocation);
     }
 }
