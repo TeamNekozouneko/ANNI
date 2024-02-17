bc935d82c10503646d0ced37b46f8109891f9efc
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2024-02-17 12:52:10 +0900

    観戦モードで弓などが使える問題を修正

--- src/main/java/net/nekozouneko/anni/ANNIPlugin.java
+++ src/main/java/net/nekozouneko/anni/ANNIPlugin.java
@@ -123,6 +123,7 @@ public final class ANNIPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerRespawnListener(), this);
+        getServer().getPluginManager().registerEvents(new ProjectileLaunchListener(), this);
 
         if (ANNIConfig.isVotifierVoteEnabled()) {
             try {
