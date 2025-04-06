e21a09a9af887425ffc0ab035ffaa1bd0ea07301
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2025-04-06 14:55:47 +0900

    スコアボードをadventure apiのComponentに変更

--- src/main/java/net/nekozouneko/anni/arena/manager/ScoreboardManager.java
+++ src/main/java/net/nekozouneko/anni/arena/manager/ScoreboardManager.java
@@ -1,13 +1,19 @@
 package net.nekozouneko.anni.arena.manager;
 
+import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.api.ViaAPI;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import fr.mrmicky.fastboard.adventure.FastBoard;
+import net.kyori.adventure.text.Component;
 import net.nekozouneko.anni.ANNIConfig;
 import net.nekozouneko.anni.ANNIPlugin;
 import net.nekozouneko.anni.arena.ANNIArena;
-import net.nekozouneko.anni.arena.ArenaState;
 import net.nekozouneko.anni.arena.team.ANNITeam;
 import net.nekozouneko.anni.board.BoardManager;
+import net.nekozouneko.anni.map.ANNIMap;
 import net.nekozouneko.anni.message.MessageManager;
-import net.nekozouneko.anni.util.CmnUtil;
+import net.nekozouneko.anni.message.TranslationManager;
+import net.nekozouneko.anni.vote.VoteManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -24,7 +30,7 @@ public class ScoreboardManager {
         this.arena = arena;
     }
 
-    public void update() {
+    /*public void update() {
         BoardManager bm = ANNIPlugin.getInstance().getBoardManager();
         MessageManager mm = ANNIPlugin.getInstance().getMessageManager();
 
@@ -42,8 +48,7 @@ public class ScoreboardManager {
                                 mm.build("scoreboard.waiting.2.more_player", (
                                         arena.getEnabledTeams().entrySet().stream()
                                                 .filter(Map.Entry::getValue)
-                                                .count() * ANNIConfig.getTeamMinPlayers()
-                                                - Bukkit.getOnlinePlayers().size())
+                                                .count() * ANNIConfig.getTeamMinPlayers() - Bukkit.getOnlinePlayers().size())
                                 );
 
                         if (arena.getMap() != null) {
@@ -115,6 +120,88 @@ public class ScoreboardManager {
                 e.printStackTrace();
             }
         }
+    }*/
+
+    public void update() {
+        BoardManager bm = ANNIPlugin.getInstance().getBoardManager();
+        TranslationManager tm = ANNIPlugin.getInstance().getTranslationManager();
+        ViaAPI<Player> viaApi = Via.getAPI();
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            try {
+                FastBoard fb = bm.get(player); //TODO
+                SimpleDateFormat df = new SimpleDateFormat(tm.string(player, "format.scoreboard_datetime"));
+                Component datetime = Component.text(df.format(Calendar.getInstance().getTime()));
+
+                switch (arena.getState()) {
+                    case WAITING -> {
+                        fb.updateTitle(tm.component(player, "scoreboard.title"));
+
+                        ANNIMap map = arena.getMap();
+                        long enabledTeams = arena.getEnabledTeams().entrySet().stream()
+                                .filter(Map.Entry::getValue)
+                                .count();
+                        long requiredPlayers = (enabledTeams * ANNIConfig.getTeamMinPlayers()) - Bukkit.getOnlinePlayers().size();
+
+                        if (map != null)
+                            fb.updateLines(tm.componentList(player, "scoreboard.not_enough.map_selected",
+                                    datetime,
+                                    requiredPlayers,
+                                    map.getName()
+                            ));
+                        else {
+                            fb.updateLines(tm.componentList(player, "scoreboard.not_enough",
+                                    datetime,
+                                    requiredPlayers,
+                                    mapEntry(player, 0),
+                                    mapEntry(player, 1),
+                                    mapEntry(player, 2)
+                            ));
+                        }
+                    }
+                    case STARTING -> {
+                        fb.updateTitle(tm.component(player, "scoreboard.title"));
+
+                        fb.updateLines(tm.componentList(player, "scoreboard.begins_soon",
+                                datetime,
+                                arena.getTimer(),
+                                arena.getMap().getName()
+                        ));
+                    }
+                    case PHASE_ONE, PHASE_TWO, PHASE_THREE, PHASE_FOUR, PHASE_FIVE, GAME_OVER -> {
+                        if (viaApi.getPlayerVersion(player) < ProtocolVersion.v1_13.getVersion()) {
+                            if (arena.getMap() != null && arena.getMap().getName() != null)
+                                fb.updateTitle(tm.component(player, "scoreboard.playing.short.title", arena.getMap().getName()));
+                            else fb.updateTitle(tm.component(player, "scoreboard.title"));
+
+                            for (ANNITeam team : ANNITeam.values()) {
+                                Integer health = arena.getNexusHealth(team);
+                                fb.updateScore(health != null ? health : 0, tm.component(player, team.getNameKey()));
+                            }
+                        }
+                        else {
+                            fb.updateTitle(tm.component(player, "scoreboard.title"));
+                            fb.updateLines(tm.componentList(player, "scoreboard.playing",
+                                    datetime,
+                                    nexusState(player, ANNITeam.RED),
+                                    nexusState(player, ANNITeam.BLUE),
+                                    nexusState(player, ANNITeam.GREEN),
+                                    nexusState(player, ANNITeam.YELLOW),
+                                    nexusHealth(player, ANNITeam.RED),
+                                    nexusHealth(player, ANNITeam.BLUE),
+                                    nexusHealth(player, ANNITeam.GREEN),
+                                    nexusHealth(player, ANNITeam.YELLOW),
+                                    arena.getMap().getName()
+                            ));
+                        }
+                    }
+                    default -> fb.updateLines(tm.componentList(player, "scoreboard.stopped", datetime));
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private String sbNexusHealth(ANNITeam team) {
@@ -134,5 +221,33 @@ public class ScoreboardManager {
 
     private int shortNexusHealth(ANNITeam team) {
         return arena.getNexusHealth(team) != null ? arena.getNexusHealth(team) : 0;
+    }
+
+    private Component nexusState(Player player, ANNITeam team) {
+        TranslationManager translation = ANNIPlugin.getInstance().getTranslationManager();
+
+        return translation.component(player, arena.isNexusLost(team) ? "scoreboard.nexus.state.lost" : "scoreboard.nexus.state.active");
+    }
+
+    private Component nexusHealth(Player player, ANNITeam team) {
+        TranslationManager translation = ANNIPlugin.getInstance().getTranslationManager();
+        Integer health = ANNIPlugin.getInstance().getCurrentGame().getNexusHealth(team);
+
+        return health != null ?
+                Component.text(String.format(translation.string(player, "format.nexus_health"), health))
+                : translation.component(player, "scoreboard.nexus.health.none");
+    }
+
+    private Component mapEntry(Player player, int index) {
+        TranslationManager translation = ANNIPlugin.getInstance().getTranslationManager();
+        VoteManager vote = ANNIPlugin.getInstance().getCurrentGame().getVoteManager();
+        List<Map.Entry<String, Integer>> results = vote.getSortedResults();
+
+        if (results.isEmpty() || results.size() <= index)
+            return translation.component(player, "scoreboard.entry", "-", "-");
+
+        Map.Entry<String, Integer> entry = results.get(index);
+
+        return translation.component(player, "scoreboard.entry", entry.getKey(), entry.getValue());
     }
 }
--- src/main/java/net/nekozouneko/anni/arena/team/ANNITeam.java
+++ src/main/java/net/nekozouneko/anni/arena/team/ANNITeam.java
@@ -3,6 +3,7 @@ package net.nekozouneko.anni.arena.team;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.md_5.bungee.api.ChatColor;
 import net.nekozouneko.anni.ANNIPlugin;
@@ -10,29 +11,26 @@ import net.nekozouneko.anni.ANNIPlugin;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+@AllArgsConstructor
 public enum ANNITeam {
 
     @SerializedName("RED")
-    RED("team.red.display", "team.red.prefix", ChatColor.RED, 'r'),
+    RED("team.red.display", "team.red.prefix", "team.red.name", ChatColor.RED, 'r'),
     @SerializedName("BLUE")
-    BLUE("team.blue.display", "team.blue.prefix", ChatColor.BLUE, 'b'),
+    BLUE("team.blue.display", "team.blue.prefix", "team.blue.name", ChatColor.BLUE, 'b'),
     @SerializedName("GREEN")
-    GREEN("team.green.display", "team.green.prefix", ChatColor.GREEN, 'g'),
+    GREEN("team.green.display", "team.green.prefix", "team.green.name", ChatColor.GREEN, 'g'),
     @SerializedName("YELLOW")
-    YELLOW("team.yellow.display", "team.yellow.prefix", ChatColor.YELLOW, 'y');
+    YELLOW("team.yellow.display", "team.yellow.prefix", "team.yellow.name", ChatColor.YELLOW, 'y');
 
     private final String name;
     private final String prefix;
+    @Getter
+    private final String nameKey;
     private final ChatColor cc;
     @Getter
     private final char bigChar;
 
-    private ANNITeam(String name, String prefix, ChatColor cc, char bigChar) {
-        this.name = name;
-        this.prefix = prefix;
-        this.cc = cc;
-        this.bigChar = bigChar;
-    }
 
     public String getTeamName() {
         return ANNIPlugin.getInstance().getMessageManager().build(name);
