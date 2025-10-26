42f86a996eaa57e34aab1c6f13161bbbd1a9d03f
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2025-10-26 13:13:48 +0900

    ChatColorを廃止する準備

--- src/main/java/net/nekozouneko/anni/arena/team/ANNITeam.java
+++ src/main/java/net/nekozouneko/anni/arena/team/ANNITeam.java
@@ -5,6 +5,7 @@ import com.google.common.collect.HashBiMap;
 import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.md_5.bungee.api.ChatColor;
 import net.nekozouneko.anni.ANNIPlugin;
 
@@ -15,21 +16,24 @@ import java.util.Map;
 public enum ANNITeam {
 
     @SerializedName("RED")
-    RED("red", "team.red.display", "team.red.prefix", "team.red.name", ChatColor.RED, 'r'),
+    RED("red", "team.red.display", "team.red.prefix", "team.red.name", ChatColor.RED, NamedTextColor.RED, 'r'),
     @SerializedName("BLUE")
-    BLUE("blue", "team.blue.display", "team.blue.prefix", "team.blue.name", ChatColor.BLUE, 'b'),
+    BLUE("blue", "team.blue.display", "team.blue.prefix", "team.blue.name", ChatColor.BLUE, NamedTextColor.BLUE, 'b'),
     @SerializedName("GREEN")
-    GREEN("green", "team.green.display", "team.green.prefix", "team.green.name", ChatColor.GREEN, 'g'),
+    GREEN("green", "team.green.display", "team.green.prefix", "team.green.name", ChatColor.GREEN, NamedTextColor.GREEN, 'g'),
     @SerializedName("YELLOW")
-    YELLOW("yellow", "team.yellow.display", "team.yellow.prefix", "team.yellow.name", ChatColor.YELLOW, 'y');
+    YELLOW("yellow", "team.yellow.display", "team.yellow.prefix", "team.yellow.name", ChatColor.YELLOW, NamedTextColor.YELLOW, 'y');
 
     @Getter
     private final String id;
     private final String name;
+    @Getter
     private final String prefix;
     @Getter
     private final String nameKey;
     private final ChatColor cc;
+    @Getter
+    private final NamedTextColor color;
     @Getter
     private final char bigChar;
 
