48fbb586614e70cd4cefa357c84a1132c93642bf
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2025-11-03 13:19:42 +0900

    キャストではない方法に変更

--- src/main/java/net/nekozouneko/anni/listener/votifier/VotifierListener.java
+++ src/main/java/net/nekozouneko/anni/listener/votifier/VotifierListener.java
@@ -7,7 +7,6 @@ import net.nekozouneko.anni.message.MessageManager;
 import net.nekozouneko.anni.util.VaultUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -34,7 +33,7 @@ public class VotifierListener implements Listener {
             VaultUtil.getEco().depositPlayer(op, ANNIConfig.getVotePoints());
         }
         if (op.isOnline()) {
-            ((Player) op).sendMessage(mm.build(
+            op.getPlayer().sendMessage(mm.build(
                     "notify.thank_you_voting",
                     Double.toString(ANNIConfig.getVotePoints()),
                     mm.build("gui.shop.ext")
