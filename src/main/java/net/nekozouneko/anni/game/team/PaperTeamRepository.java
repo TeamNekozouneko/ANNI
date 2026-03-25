acac3089408bbea949e2408c415a44ad0d5ad44a
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2026-03-25 15:27:10 +0900

    自チームに対してネームタグを隠していた問題を修正

--- src/main/java/net/nekozouneko/anni/game/team/PaperTeamRepository.java
+++ src/main/java/net/nekozouneko/anni/game/team/PaperTeamRepository.java
@@ -34,7 +34,7 @@ public class PaperTeamRepository implements TeamRepository {
         team.color(color.getColor());
         team.setAllowFriendlyFire(false);
         team.setCanSeeFriendlyInvisibles(true);
-        team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.FOR_OWN_TEAM);
+        team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.FOR_OTHER_TEAMS);
 
         teams.put(color, team);
     }
